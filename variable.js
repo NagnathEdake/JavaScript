6d153555fa0038b3f9ed0a13c23eb14701122d89
Nagnath Edake <nagnathedake817@gmail.com>
2023-12-14 19:13:37 +0530

    new variable

--- variable.js
+++ variable.js
@@ -2,7 +2,7 @@ const accountId = 14342;
 let accountEmail = "demo@gmail.com";
 let accountPass = "2341343532";
 let accountCity = "solapur";
-
+let accountState;
 console.log(accountId);
 
-console.table([accountId , accountEmail , accountPass , accountCity]);
+console.table([accountId , accountEmail , accountPass , accountCity , accountState]);
