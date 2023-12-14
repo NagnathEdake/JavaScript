44ddb223422d1d061dd623363f941c210063226b
Nagnath Edake <nagnathedake817@gmail.com>
2023-12-14 19:12:13 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,8 @@
+const accountId = 14342;
+let accountEmail = "demo@gmail.com";
+let accountPass = "2341343532";
+let accountCity = "solapur";
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPass , accountCity]);
